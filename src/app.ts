98bd8a0ed5e7222ee90c0b6fa144c22ffc9c8058
patrickDesign <patrickwees@gmail.com>
2020-01-14 20:26:15 -0800

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,3 @@ const server = new GraphQLServer({typeDefs, resolvers})
 const PORT = process.env.PORT ? process.env.PORT : 3000
 
 server.start({ port: PORT }, () => console.log(`Example app listening on port ${PORT}!`))
-
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("Hi evan...");
-// });
